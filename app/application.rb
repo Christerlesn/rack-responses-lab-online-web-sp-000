f4073ad553a57d0e6b5411a854b9ec5a17def665
Christerle Nounoute <christerlesn@gmail.com>
2021-01-10 20:50:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,12 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello"
+
+    t = Time.now
+    time_hour = t.hour
+    time_min = t.min 
+    current_time = "#{time_hour}:#{time_min}"
+    resp.write "The time is #{current_time}"
     resp.finish
   end
 end
